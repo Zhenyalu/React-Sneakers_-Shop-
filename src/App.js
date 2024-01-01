5f02f7b7fbf4beff37beb7f9ea222ac09335d8a5
Yevhen Luchko <luchko.genya@gmail.com>
2024-01-01 18:40:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrapper">
+      <header>
+       <div className="headerLeft">
+       <img src="/img/logo.svg" alt="Logo"/>
+        <div className="headerInfo">
+          <h3>REACT SNEAKERS</h3>
+          <p>Магазин лучших кроссовок</p>
+        </div>
+       </div>
+        <ul className="headerRight">
+          <li>
+            <img src="/img/cart.svg"/>
+            <span>1205 Euro.</span>
+            </li>
+          <li>
+            <img src="/img/favorite.svg"/>
+            <span>Закладки</span>
+            </li>
+          <li>
+            <img src="/img/union.svg"/>
+            <span>Профиль</span>
+            </li>
+        </ul>
       </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+
+       <div className="sneakers">
+       <div className="card">
+          <img src="/img/nike_green.svg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="cardOrder">
+            <div className="cardOrderTwo">
+              <span>Цена:</span>
+              <b>12 999 Euro.</b>
+            </div>
+            <button><b>+</b></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/nike_white.svg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="cardOrder">
+            <div className="cardOrderTwo">
+              <span>Цена:</span>
+              <b>12 999 Euro.</b>
+            </div>
+            <button><b>+</b></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/nike_multicolor.svg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="cardOrder">
+            <div className="cardOrderTwo">
+              <span>Цена:</span>
+              <b>12 999 Euro.</b>
+            </div>
+            <button><b>+</b></button>
+          </div>
+        </div>
+        <div className="card">
+          <img src="/img/nike_yellow.svg" alt=""/>
+          <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+          <div className="cardOrder">
+            <div className="cardOrderTwo">
+              <span>Цена:</span>
+              <b>12 999 Euro.</b>
+            </div>
+            <button><b>+</b></button>
+          </div>
+        </div>
+       </div>
+
+      </div>
     </div>
   );
 }
